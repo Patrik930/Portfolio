67cd3e01bf6914a059ab001dd2081c1c379e5cdb
Patrik930 <focimc02@gmail.com>
2024-02-07 14:44:55 +0100

    Everything updated

--- projects.js
+++ projects.js
@@ -7,5 +7,21 @@ export const projects =()=>{
 }
 
 function renderProjects(data){
-    console.log(data);
+
+    data.forEach(e => {
+        console.log(e)
+        document.querySelector(".projectsData").innerHTML+=`
+        <div class="row">
+        <div class="card text-center col">
+          <div class="card-header">${e.name}</div>
+          <div class="card-body">
+            <h5 class="card-title">${e.descr}</h5>
+
+            <a href="${e.github}" target="_blank" class="btn btn-primary"><i class="fa-brands fa-github"></i></a>
+            <a href="${e.hosted}" target="_blank" class="btn btn-primary">Go to the website</a>
+            <img src="${e.img}" alt="${e.name}"/>
+          </div>
+        </div>
+        `
+    });
 }
